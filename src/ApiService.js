e724e1f8098284f8f110eacbb4d8302d2b42ea3c
csh <csh@duksung.ac.kr>
2024-05-13 17:44:32 +0900

    완료: 20240513 수업

--- src/ApiService.js
+++ src/ApiService.js
@@ -1,10 +1,16 @@
 import { API_BASE_URL } from "./api-config";
 
 export function call(api, method, request) {
+    let headers = new Headers({
+        "Content-Type": "application/json"
+    });
+
+    const accessToke = localStorage.getItem("ACCESS_TOKEN");
+    if(accessToke && accessToke != null) {
+        headers.append("Authorization", "Bearer " + accessToke);
+    }
     let options = {
-        headers: new Headers({
-            "Content-Type": "application/json",
-        }),
+        headers: headers,
         url: API_BASE_URL + api,
         method: method,
     };
@@ -16,9 +22,26 @@ export function call(api, method, request) {
     return fetch(options.url, options).then( (response) => {
         if( response.status === 200 ) {
             return response.json();
+        } else if( response.status === 403) {
+            window.location.href = "/login";
+        } else {
+            Promise.reject(response);
+            throw Error(response);
         }
     } ).catch((error)=> {
         console.log("http error");
         console.log(error);
     });
+}
+
+
+// 로그인하기
+export function signin(userDTO) {
+    return call("/auth/signin", "POST", userDTO)
+        .then( (response) => {
+            if( response.token ) {
+                localStorage.setItem("ACCESS_TOKEN", response.token);
+                window.location.href = "/";
+            }
+        })
 }
--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,61 @@
 import React from "react";
+import {Container, Grid, Typography, TextField, Button} from "@mui/material";
+import {signin} from "./ApiService";
 
 const Login = () => {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        
+        const data = new FormData(event.target);
+        const username = data.get("username");
+        const passwrod = data.get("password");
+        
+        signin({username:username, password: passwrod});
+    }
+
     return(
-        <p>로그인 페이지</p>
+        <Container component="main" maxWidth="xs" style={{marginTop: "8%"}}>
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                    <Typography component="h1" variant="h5">
+                        로그인
+                    </Typography>
+                </Grid>
+            </Grid>
+            <form noValidate onSubmit={handleSubmit}>
+                {" "}
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                        <TextField
+                            variant="outlined"
+                            required
+                            fullWidth
+                            id="username"
+                            label="아이디"
+                            name="username"
+                            autoComplete="username"
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            variant="outlined"
+                            required
+                            fullWidth
+                            id="password"
+                            label="패스워드"
+                            name="password"
+                            autoComplete="current-password"
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Button type="submit" fullWidth variant="contained" color="primary">
+                            로그인
+                        </Button>
+                    </Grid>
+                </Grid>
+            </form>
+        </Container>
+
     );
 }
 
